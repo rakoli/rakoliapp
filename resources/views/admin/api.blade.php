9f222fb6c902f088e107840d58d06a41e2730a68
unknown <famshdeveloper@gmail.com>
2023-10-06 17:20:05 +0300

    Admin pages added

---
+++ resources/views/admin/api.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <!--begin::Main column-->
+    <div class="d-flex flex-column flex-lg-row-fluid gap-7 gap-lg-10">
+        <!--begin::Order details-->
+
+        <!--begin::Order details-->
+        <div class="card card-flush py-4">
+            <!--begin::Card header-->
+            <div class="card-header">
+                <div class="card-title">
+                    <h2>API</h2>
+                </div>
+            </div>
+            <!--end::Card header-->
+            <!--begin::Card body-->
+            <div class="card-body pt-0">
+                <h3>No Contents</h3>
+            </div>
+            <!--end::Card body-->
+        </div>
+        <!--end::Order details-->
+
+    </div>
+    <!--end::Main column-->
+@endsection
