37056728adf64bd3d6efdee2764eb48005de0565
Erick Boni <emabusi@gmail.com>
2023-10-15 20:38:58 +0300

    Added bugsnag package and TestController

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Bugsnag\BugsnagLaravel\Facades\Bugsnag;
+use RuntimeException;
+
+class TestController extends Controller
+{
+    public function testing()
+    {
+
+
+    }
+}
