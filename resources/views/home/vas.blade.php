69cb05107333ab28c7c8e66dcddde9aad18241de
Erick Boni <emabusi@gmail.com>
2023-10-21 00:03:03 +0300

    VAS and Admin dashboard update

---
+++ resources/views/home/vas.blade.php
@@ -0,0 +1,13 @@
+@section('title', "VAS - Dashboard")
+
+@section('content')
+
+    <!--begin::Container-->
+    <div id="kt_content_container" class="container-xxl">
+
+
+
+    </div>
+    <!--end::Container-->
+
+@endsection
