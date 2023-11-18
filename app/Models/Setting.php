b6de5faeed36a746e2e452f57728404ee754eb14
Erick Boni <emabusi@gmail.com>
2023-11-18 11:44:58 +0300

    setting fix

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -10,21 +10,4 @@ class Setting extends Model
 {
     use HasFactory;
 
-    public static function booted()
-    {
-
-        static::created(fn(Setting $setting) => SettingDataStore::load($setting));
-
-        static::updated(function (Setting $setting){
-
-            SettingDataStore::burst($setting);
-
-            SettingDataStore::load($setting);
-
-        });
-
-        static::deleted(fn(Setting $setting)=> SettingDataStore::burst($setting));
-
-    }
-
 }
