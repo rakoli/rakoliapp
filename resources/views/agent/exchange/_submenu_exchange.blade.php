40c51ef93b80787997ba26fa09f558a227a8c6e2
Erick Boni <emabusi@gmail.com>
2023-11-26 11:09:03 +0300

    exchange submenu update

--- resources/views/agent/exchange/_submenu_exchange.blade.php
+++ resources/views/agent/exchange/_submenu_exchange.blade.php
@@ -8,31 +8,31 @@
             <ul class="nav flex-wrap border-transparent fw-bold">
                 <!--begin::Nav item-->
                 <li class="nav-item my-1">
-                    <a class="btn btn-color-gray-600 btn-active-secondary btn-active-color-primary fw-bolder fs-8 fs-lg-base nav-link px-3 px-lg-8 mx-1 text-uppercase {{returnActiveSubMenuStyle('ads')}}"
+                    <a class="btn btn-color-gray-600 btn-active-secondary btn-active-color-primary fw-bolder fs-8 fs-lg-base nav-link px-3 px-lg-8 mx-1 {{returnActiveSubMenuStyle('ads')}}"
                        href="{{route('exchange.ads')}}">{{__('Ads')}}</a>
                 </li>
                 <!--end::Nav item-->
                 <!--begin::Nav item-->
                 <li class="nav-item my-1">
-                    <a class="btn btn-color-gray-600 btn-active-secondary btn-active-color-primary fw-bolder fs-8 fs-lg-base nav-link px-3 px-lg-8 mx-1 text-uppercase {{returnActiveSubMenuStyle('orders')}}"
+                    <a class="btn btn-color-gray-600 btn-active-secondary btn-active-color-primary fw-bolder fs-8 fs-lg-base nav-link px-3 px-lg-8 mx-1 {{returnActiveSubMenuStyle('orders')}}"
                        href="{{route('exchange.orders')}}">{{__('Orders')}}</a>
                 </li>
                 <!--end::Nav item-->
                 <!--begin::Nav item-->
                 <li class="nav-item my-1">
-                    <a class="btn btn-color-gray-600 btn-active-secondary btn-active-color-primary fw-bolder fs-8 fs-lg-base nav-link px-3 px-lg-8 mx-1 text-uppercase {{returnActiveSubMenuStyle('transactions')}}"
+                    <a class="btn btn-color-gray-600 btn-active-secondary btn-active-color-primary fw-bolder fs-8 fs-lg-base nav-link px-3 px-lg-8 mx-1 {{returnActiveSubMenuStyle('transactions')}}"
                        href="{{route('exchange.transactions')}}">{{__('Transactions')}}</a>
                 </li>
                 <!--end::Nav item-->
                 <!--begin::Nav item-->
                 <li class="nav-item my-1">
-                    <a class="btn btn-color-gray-600 btn-active-secondary btn-active-color-primary fw-bolder fs-8 fs-lg-base nav-link px-3 px-lg-8 mx-1 text-uppercase {{returnActiveSubMenuStyle('posts')}}"
+                    <a class="btn btn-color-gray-600 btn-active-secondary btn-active-color-primary fw-bolder fs-8 fs-lg-base nav-link px-3 px-lg-8 mx-1 {{returnActiveSubMenuStyle('posts')}}"
                        href="{{route('exchange.posts')}}">{{__('Posts')}}</a>
                 </li>
                 <!--end::Nav item-->
                 <!--begin::Nav item-->
                 <li class="nav-item my-1">
-                    <a class="btn btn-color-gray-600 btn-active-secondary btn-active-color-primary fw-bolder fs-8 fs-lg-base nav-link px-3 px-lg-8 mx-1 text-uppercase {{returnActiveSubMenuStyle('security')}}"
+                    <a class="btn btn-color-gray-600 btn-active-secondary btn-active-color-primary fw-bolder fs-8 fs-lg-base nav-link px-3 px-lg-8 mx-1 {{returnActiveSubMenuStyle('security')}}"
                        href="{{route('exchange.security')}}">{{__('Security')}}</a>
                 </li>
                 <!--end::Nav item-->
