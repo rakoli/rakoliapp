70d6290538de99d542877715fc049122d735b12a
Erick Boni <emabusi@gmail.com>
2023-12-03 10:19:28 +0300

    Tests minor updates

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,17 +3,23 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
+    use RefreshDatabase, WithFaker;
+
+    /** @test */
+    public function guests_are_redirected_to_login_page()
     {
+        // Arrange
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        // Assert
+        $response->assertRedirect('/login');
     }
+
 }
