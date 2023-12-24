9961a6cbb5c88e94a1f6c2abe2eb22e8f96aa25f
Erick Boni <emabusi@gmail.com>
2023-12-24 11:35:41 +0300

    Name user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\Business;
+use App\Utils\Enums\UserTypeEnum;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -10,30 +13,25 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+
     public function definition(): array
     {
+        $businesses = Business::get('code')->toArray();
+        $fname = fake()->firstName;
+        $lname = fake()->lastName;
+        $name = "$fname $lname";
+
         return [
-            'fname' => fake()->firstName(),
-            'lname' => fake()->lastName(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'country_code' => 'TZ',
+            'fname' => $fname,
+            'lname' => $lname,
+            'phone' => '255766'.fake()->numerify("######"),
+            'email' => fake()->email,
+            'code' => generateCode($name),
+            'password' => Hash::make('12345678'),
+            'type' => UserTypeEnum::AGENT->value,
+            'business_code' => fake()->randomElement($businesses)['code'],
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
 }
