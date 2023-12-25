7cc8c8d64150ca4814607b49c1838a692c3bc98a
Erick Boni <emabusi@gmail.com>
2023-12-25 15:12:40 +0300

    Migration bug fixing

---
+++ database/factories/BusinessFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Business;
+use App\Models\Package;
+use App\Utils\Enums\BusinessStatusEnum;
+use App\Utils\Enums\BusinessTypeEnum;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Business>
+ */
+class BusinessFactory extends Factory
+{
+    protected $model = Business::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $referral = Business::first();
+        $referralCode = null;
+        if($referral != null){
+            $referralCode = $referral->code;
+        }
+        $packages = Package::get('code')->toArray();
+        return [
+            'country_code' => fake()->randomElement(['TZ', 'KE']),
+            'referral_business_code' => $referralCode,
+            'code' => Str::random(10),
+            'type' => BusinessTypeEnum::AGENCY,
+            'business_name' => fake()->company,
+            'status' => fake()->randomElement(BusinessStatusEnum::class),
+            'package_code' => fake()->randomElement($packages)['code'],
+            'package_expiry_at' => now()->addDays(random_int(1,30)),
+            'business_location' => fake()->address,
+            'last_seen' => now()->subMinutes(random_int(1,180)),
+        ];
+    }
+}
