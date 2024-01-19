cf7e98331936ba4e85cc395f47cdadf7ea1e1e3a
unknown <yash.smarttechnica@gmail.com>
2024-01-19 15:49:47 +0530

    changes as per client meeting in finance page

--- app/Http/Controllers/Agent/PaymentController.php
+++ app/Http/Controllers/Agent/PaymentController.php
@@ -7,6 +7,7 @@ use App\Models\Business;
 use App\Models\BusinessWithdrawMethod;
 use App\Models\Location;
 use App\Models\Transaction;
+use App\Models\WithdrawRequest;
 use Illuminate\Http\Request;
 use Yajra\DataTables\DataTables;
 
@@ -24,7 +25,7 @@ class PaymentController extends Controller
             $orderBy = ['order_by' => $request->get('order_by')];
             $orderByFilter = $request->get('order_by');
         }
-        $roles = Transaction::where('business_code', $user->business_code)->orderBy('id', 'desc');
+        $roles = WithdrawRequest::where('business_code', $user->business_code)->orderBy('id', 'desc');
         $balance = Business::where('code', $user->business_code)->get();
         $existingData = BusinessWithdrawMethod::where('business_code', $user->business_code)->get();
 
@@ -43,13 +44,18 @@ class PaymentController extends Controller
         // DataTable
         $dataTableHtml = $builder->columns([
             ['data' => 'id', 'title' => __('ID')],
-            ['data' => 'business_code', 'title' => __('Business Code')],
-            ['data' => 'type', 'title' => __('Type')],
-            ['data' => 'category', 'title' => __('Category')],
+            ['data' => 'method_ac_number', 'title' => __('Method AC Number')],
             ['data' => 'amount', 'title' => __('Amount')],
             ['data' => 'amount_currency', 'title' => __('Amount Currency')],
-            ['data' => 'description', 'title' => __('Description')],
-            // ['data' => 'note', 'title' => __('Note')],
+            ['data' => 'status', 'title' => __('Status')],
+            ['data' => 'completion_note', 'title' => __('Completion Note')],
+            [
+                'data' => 'created_at',
+                'title' => __('Created At'),
+                'render' => 'function (data) {
+                    return moment(data).format("DD-MM-YYYY HH:mm:ss");
+                }'
+            ],
             // ['data' => 'actions', 'title' => __('Actions')],
         ])->responsive(true)
             ->ordering(false)
@@ -99,6 +105,12 @@ class PaymentController extends Controller
             'amount' => 'required',
         ]);
 
+        $get_withdraw_method = BusinessWithdrawMethod::where('business_code',$request->user()->business_code)->first();
+        
+        if (!$get_withdraw_method) {
+            return redirect()->back()->with(['message' => __('Please add withdrawal method')]);
+        }
+
         $user = $request->user();
         $currency = session('currency');
         if ($currency == null) {
@@ -109,23 +121,16 @@ class PaymentController extends Controller
         }else{
             $description = '';
         }
-        $old_balance = $user->business->balance;
-        $new_balance = $old_balance - $request->amount;
-        Business::where('code',$request->user()->business_code)->update(['balance' => $new_balance]);
-        $location_code = Location::where('business_code',$request->user()->business_code)->first();
 
-        $transaction = new Transaction();
-        $transaction->business_code = $request->user()->business_code;
-        $transaction->location_code = $location_code->code;
-        $transaction->user_code = $request->user()->code;
-        $transaction->type = 'OUT';
-        $transaction->category = $request->category;
-        $transaction->amount = $request->amount;
-        $transaction->amount_currency = $currency;
-        $transaction->balance_old = $old_balance;
-        $transaction->balance_new = $new_balance;
-        $transaction->description = $description;
-        $transaction->save();
+        $withdraw = new WithdrawRequest();
+        $withdraw->business_code = $request->user()->business_code;
+        $withdraw->method_name = $get_withdraw_method->method_name;
+        $withdraw->method_ac_name = $get_withdraw_method->method_ac_name;
+        $withdraw->method_ac_number = $get_withdraw_method->method_ac_number;
+        $withdraw->amount = $request->amount;
+        $withdraw->amount_currency = $currency;
+        $withdraw->completion_note = $description;
+        $withdraw->save();
 
         return redirect()->back()->with(['message' => __('Withdraw Fund') . ' ' . __('Added Successfully')]);
     }
