3fc18a4e71c10298ffa731253c0f16c5d52cf454
Erick Boni <emabusi@gmail.com>
2024-01-31 21:44:36 +0300

    Added run database transaction helper function

--- app/helpers.php
+++ app/helpers.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 use App\Models\User;
+use Bugsnag\BugsnagLaravel\Facades\Bugsnag;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
 
 if (!function_exists('settings')) {
@@ -188,4 +191,31 @@ function tradeOrderInvence($type) : string
 function idNumberDisplay($number)
 {
     return str_pad("$number", 5,'0',STR_PAD_LEFT);
+}
+
+
+function runDatabaseTransaction(\Closure $closure)
+{
+    DB::beginTransaction();
+
+    try {
+        // Execute the provided closure
+        $result = $closure();
+
+        // If the closure execution is successful, commit the transaction
+        DB::commit();
+
+        return $result; // Return the result of the closure if needed
+    } catch (\Exception $exception) {
+        // If an exception occurs, rollback the transaction
+        DB::rollback();
+
+        // Log the exception
+        Log::debug("ERROR: " . $exception->getMessage());
+
+        // Notify Bugsnag about the exception
+        Bugsnag::notifyException($exception);
+
+        return false; // Return false or handle the error as needed
+    }
 }
