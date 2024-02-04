f027be73397a3cc67a110f568f6a3891649eed22
Erick Boni <emabusi@gmail.com>
2024-02-05 00:34:07 +0300

    merge bugfix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
-        Model::preventLazyLoading(! $this->app->isProduction());
+//        Model::preventLazyLoading(! $this->app->isProduction());
     }
 }
