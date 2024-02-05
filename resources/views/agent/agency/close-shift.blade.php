541773f1b69e10f7e3a74ec3a60925e38b258305
Njuguna Philip <philipnjuguna66@gmail.com>
2024-02-05 10:29:31 +0300

    upload National id, select might not be working before

--- resources/views/agent/agency/close-shift.blade.php
+++ resources/views/agent/agency/close-shift.blade.php
@@ -24,7 +24,7 @@
                 <div class="card mb-5 mb-xl-8">
 
                     <div class="card-body">
-                        @include('agent.agency.shift.close-shift')
+                        @include('agent.agency.shift.close_shift_form')
                     </div>
                 </div>
 
@@ -62,7 +62,7 @@
                                         <!--begin::Number-->
                                         <div class="d-flex align-items-center">
 
-                                            <div class="fs-4qx fw-normal counted text-success" data-kt-countup="true"
+                                            <div class="fs-5qx fw-normal counted text-success" data-kt-countup="true"
                                                  data-kt-countup-value="{{ number_format($totalBalance ?? 0 , 2) }}"
                                                  data-kt-countup-prefix="{{ currencyCode() }}" data-kt-initialized="1">
                                                 <span class="">   {{ Illuminate\Support\Number::abbreviate($totalBalance ?? 0 ,3) }}</span>
@@ -94,6 +94,25 @@
                                     </div>
                                     <!--end::Stat-->
 
+                                    <!--begin::Stat-->
+                                    <div class="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3">
+                                        <!--begin::Number-->
+                                        <div class="d-flex align-items-center">
+
+                                            <div class="fs-2 fw-bolder counted fs-3qx text-gray-800" data-kt-countup="true"
+                                                 data-kt-countup-value="{{ number_format($totalBalance - $shift->cash_end , 2) }}"
+                                                 data-kt-countup-prefix="{{ currencyCode() }}" data-kt-initialized="1">
+                                                {{ Illuminate\Support\Number::abbreviate($totalBalance - $shift->cash_end , 3) }}
+                                            </div>
+                                        </div>
+                                        <!--end::Number-->
+
+                                        <!--begin::Label-->
+                                        <div class="fw-semibold fw-bolder  fs-6 text-gray-500">{{ __('Till Balances ') }}</div>
+                                        <!--end::Label-->
+                                    </div>
+                                    <!--end::Stat-->
+
 
                                 </div>
                                 <!--end::Stats-->
@@ -110,7 +129,6 @@
                                     <thead>
                                     <tr>
                                         <th>Till</th>
-                                        <th>{{ __('Start Balance') }}</th>
                                         <th>{{ __('End Balance') }}</th>
                                         <th>{{ __('') }}</th>
                                     </tr>
@@ -119,7 +137,7 @@
                                     @foreach($tills as $till)
                                         <tr>
                                             <td>{{  __($till->network?->agency?->name) }}</td>
-                                            <td>{{  Number::currency($till->balance_old,  currencyCode())}}</td>
+
                                             <td>{{  Number::currency($till->balance_new,  currencyCode())}}</td>
                                         </tr>
                                     @endforeach
