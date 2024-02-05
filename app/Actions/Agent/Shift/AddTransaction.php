9e9c2e2449894712a648f69b30d0c692ee6ca774
Njuguna Philip <philipnjuguna66@gmail.com>
2024-02-05 08:45:29 +0300

    Add Transaction using Database Transaction Method

--- app/Actions/Agent/Shift/AddTransaction.php
+++ app/Actions/Agent/Shift/AddTransaction.php
@@ -22,33 +22,26 @@ class AddTransaction
     public function handle(Shift $shift, array $data)
     {
 
-        try {
 
-            throw_if(condition: ! Shift::query()->where('status', ShiftStatusEnum::OPEN)->exists(),
-                exception: new \Exception('You cannot transact without an open shift')
-            );
+        return runDatabaseTransaction(function ()  use ( $shift, $data) {
 
-            DB::beginTransaction();
 
-            [$newBalance, $oldBalance, $till] = match ($data['type']) {
-                TransactionTypeEnum::MONEY_IN->value => AddTransaction::moneyIn($data),
-                TransactionTypeEnum::MONEY_OUT->value => AddTransaction::moneyOut($data),
-            };
 
-            $this->createShiftTransaction(
-                shift: $shift,
-                data: $data,
-                oldBalance: $till->balance_old,
-                newBalance: $till->balance_new
-            );
+                [$newBalance, $oldBalance, $till] = match ($data['type']) {
+                    TransactionTypeEnum::MONEY_IN->value => AddTransaction::moneyIn($data),
+                    TransactionTypeEnum::MONEY_OUT->value => AddTransaction::moneyOut($data),
+                };
 
-            DB::commit();
+                $this->createShiftTransaction(
+                    shift: $shift,
+                    data: $data,
+                    oldBalance: $till->balance_old,
+                    newBalance: $till->balance_new
+                );
 
-        } catch (\Exception $e) {
 
-            DB::rollBack();
-            throw new \Exception($e->getMessage());
-        }
+        });
+
 
     }
 }
