45247e6b465ab8dcccc886d341314dd12c1e7d3b
Erick Boni <emabusi@gmail.com>
2024-02-05 21:45:44 +0300

    final reg steps checks

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -45,6 +45,7 @@ class LoginController extends Controller
     /**
      * Log the user out of the application.
      *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\RedirectResponse
      */
     public function logout(Request $request)
