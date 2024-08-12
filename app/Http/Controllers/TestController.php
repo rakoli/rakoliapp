c003e12b41da98ecd00f6c302e90988bcfb10e1e
Erick Boni <emabusi@gmail.com>
2024-08-12 12:23:08 +0300

    update registration complete message

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,13 +7,29 @@ use App\Models\Package;
 use App\Models\PackageAvailableFeatures;
 use App\Models\PackageFeature;
 use App\Models\PackageName;
+use App\Models\User;
 use App\Utils\Enums\FeatureAccessEnum;
 use App\Utils\SelcomPayment;
+use App\Utils\SMS;
+use Illuminate\Support\Facades\Log;
 
 class TestController extends Controller
 {
     public function testing()
     {
+        $users = User::where('registration_step',3)->get();
+
+        foreach ($users as $user) {
+
+            $text = "Ndugu {$user->fname}, Rakoli sasa ina package ya bure (trial) unayoweza kutumia kwa mwaka moja. Malizia usajili wako na package hii kuimarisha biashara yako ya uwakala. Rakoli - Thamani kwa Wakala";
+
+            if(env('APP_ENV') == 'production'){
+                SMS::sendToUser($user, $text);
+            }else{
+                Log::debug("SMS: $text");
+            }
+
+        }
 
 
         return false;
