d0880fc78618c3f41aeae504793c912ace56dfec
Erick Boni <emabusi@gmail.com>
2024-08-12 12:39:59 +0300

    trialsms stop

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -17,19 +17,19 @@ class TestController extends Controller
 {
     public function testing()
     {
-        $users = User::where('registration_step',3)->get();
-
-        foreach ($users as $user) {
-
-            $text = "Ndugu {$user->fname}, Rakoli sasa ina package ya bure (trial) unayoweza kutumia kwa mwaka moja. Malizia usajili wako na package hii kuimarisha biashara yako ya uwakala. Rakoli - Thamani kwa Wakala";
-
-            if(env('APP_ENV') == 'production'){
-                SMS::sendToUser($user, $text);
-            }else{
-                Log::debug("SMS: $text");
-            }
-
-        }
+//        $users = User::where('registration_step',3)->get();
+//
+//        foreach ($users as $user) {
+//
+//            $text = "Ndugu {$user->fname}, Rakoli sasa ina package ya bure (trial) unayoweza kutumia kwa mwaka moja. Malizia usajili wako na package hii kuimarisha biashara yako ya uwakala. Rakoli - Thamani kwa Wakala";
+//
+//            if(env('APP_ENV') == 'production'){
+//                SMS::sendToUser($user, $text);
+//            }else{
+//                Log::debug("SMS: $text");
+//            }
+//
+//        }
 
 
         return false;
