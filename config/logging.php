dca0c44171f8db36ea2a9859d3cbb5363b391227
Ravi Mehta <rvmehta745@gmail.com>
2024-08-14 14:16:09 +0530

    set log channel to daily

--- config/logging.php
+++ config/logging.php
@@ -54,7 +54,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single', 'bugsnag'],
+            'channels' => ['daily', 'bugsnag'],
             'ignore_exceptions' => false,
         ],
 
