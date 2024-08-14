cd0bf0d8e05d9e9f0aa52bdc402175f797da306f
Ravi Mehta <rvmehta745@gmail.com>
2024-08-14 14:29:17 +0530

    add data on homepage

--- resources/views/dashboard/agent.blade.php
+++ resources/views/dashboard/agent.blade.php
@@ -5,7 +5,10 @@
 @section('header_js')
     <link href="{{asset('assets/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet" type="text/css" />
 @endsection
-
+<style>
+    .d-flex.flex-column.content-justify-center.flex-row-fluid{width:100%}
+    .align-start{align-items: flex-start;}
+</style>
 @section('content')
 
     <!--begin::Container-->
@@ -13,10 +16,75 @@
 
 
         <!--begin::Row-->
-        <div class="row g-5 gx-xl-10 mb-5 mb-xl-10">
+        <div class="row g-5 gx-xl-10 mb-5 mb-xl-10 align-start">
 
             <!--begin::Col-->
             <div class="col-md-6 col-lg-6 col-xl-6 col-xxl-3 mb-md-5 mb-xl-10">
+                <!--begin::List widget 4-->
+                <div class="card card-flush h-md-50 mb-5 mb-xl-10">
+                    <!--begin::Header-->
+                    <div class="card-header pt-5">
+                        <!--begin::Title-->
+                        <h3 class="card-title align-items-start flex-column">
+                            <span class="card-label fw-bold text-dark">{{__('Highlights')}}</span>
+                            <span class="text-gray-400 mt-1 fw-semibold fs-6">{{__('30 days financials')}}</span>
+                        </h3>
+                        <!--end::Title-->
+                    </div>
+                    <!--end::Header-->
+                    <!--begin::Body-->
+                    <div class="card-body pt-5">
+                        <!--begin::Item-->
+                        <div class="d-flex flex-stack">
+                            <!--begin::Section-->
+                            <div class="text-gray-700 fw-semibold fs-6 me-2">{{__('Income')}}</div>
+                            <!--end::Section-->
+                            <!--begin::Statistics-->
+                            <div class="d-flex align-items-senter">
+                                <!--begin::Number-->
+                                <span class="text-gray-900 fw-bolder fs-6">{{session('currency').' '.$stats['highlights']['income']}}</span>
+                            </div>
+                            <!--end::Statistics-->
+                        </div>
+                        <!--end::Item-->
+                        <!--begin::Separator-->
+                        <div class="separator separator-dashed my-3"></div>
+                        <!--end::Separator-->
+                        <!--begin::Item-->
+                        <div class="d-flex flex-stack">
+                            <!--begin::Section-->
+                            <div class="text-gray-700 fw-semibold fs-6 me-2">{{__('Expenses')}}</div>
+                            <!--end::Section-->
+                            <!--begin::Statistics-->
+                            <div class="d-flex align-items-senter">
+                                <!--begin::Number-->
+                                <span class="text-gray-900 fw-bolder fs-6">{{session('currency').' '.$stats['highlights']['expense']}}</span>
+                                <!--end::Number-->
+                            </div>
+                            <!--end::Statistics-->
+                        </div>
+                        <!--end::Item-->
+                        <!--begin::Separator-->
+                        <div class="separator separator-dashed my-3"></div>
+                        <!--end::Separator-->
+                        <!--begin::Item-->
+                        <div class="d-flex flex-stack">
+                            <!--begin::Section-->
+                            <div class="text-gray-700 fw-semibold fs-6 me-2">{{__('Referrals')}}</div>
+                            <!--end::Section-->
+                            <!--begin::Statistics-->
+                            <div class="d-flex align-items-senter">
+                                <!--begin::Number-->
+                                <span class="text-gray-900 fw-bolder fs-6">{{$stats['highlights']['referrals']}}</span>
+                                <!--end::Number-->
+                            </div>
+                            <!--end::Statistics-->
+                        </div>
+                        <!--end::Item-->
+                    </div>
+                    <!--end::Body-->
+                </div>
+                <!--end::LIst widget 4-->
                 <!--begin::Card widget 1-->
                 <div class="card card-flush bgi-no-repeat bgi-size-contain bgi-position-x-end h-md-50 mb-5 mb-xl-10">
                     <!--begin::Header-->
@@ -46,6 +114,7 @@
                     <!--end::Card body-->
                 </div>
                 <!--end::Card widget 1-->
+                
                 <!--begin::Card widget 2-->
                 <div class="card card-flush h-md-50 mb-5 mb-xl-10">
                     <!--begin::Header-->
@@ -63,7 +132,7 @@
                     </div>
                     <!--end::Header-->
                     <!--begin::Card body-->
-                    <div class="card-body d-flex flex-column justify-content-end pe-0">
+                    <div class="card-body d-flex align-items-end pt-0">
                         <!--begin::Progress-->
                         <div class="d-flex align-items-center flex-column mt-3 w-100">
                             <div class="d-flex justify-content-between fw-bold fs-6 w-100 mt-auto mb-2">
@@ -75,12 +144,78 @@
                     <!--end::Card body-->
                 </div>
                 <!--end::Card widget 2-->
+                
             </div>
             <!--end::Col-->
 
             <!--begin::Col-->
             <div class="col-md-6 col-lg-6 col-xl-6 col-xxl-3 mb-md-5 mb-xl-10">
 
+
+                <!--begin::Card widget 3-->
+                <div class="card card-flush h-md-50 mb-5 mb-xl-10">
+                    <!--begin::Header-->
+                    <div class="card-header pt-5">
+                        <!--begin::Title-->
+                        <div class="card-title d-flex flex-column">
+                            <!--begin::Info-->
+                            <div class="d-flex align-items-center">
+                                <!--begin::Currency-->
+                                {{-- <span class="fs-4 fw-semibold text-gray-400 me-1 align-self-start">{{session('currency')}}</span> --}}
+                                <!--end::Currency-->
+                                <!--begin::Amount-->
+                                <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">{{$stats['total_transaction']}}</span>
+                                <!--end::Amount-->
+                            </div>
+                            <!--end::Info-->
+                            <!--begin::Subtitle-->
+                            <span class="text-gray-400 pt-1 fw-semibold fs-6">{{__('Total Transaction')}}</span>
+                            <!--end::Subtitle-->
+                        </div>
+                        <!--end::Title-->
+                    </div>
+                    <!--end::Header-->
+                    <!--begin::Card body-->
+                    <div class="card-body pt-2 pb-4 d-flex flex-wrap align-items-center">
+                        <!--begin::Chart-->
+                        <div class="d-flex flex-center me-5 pt-2">
+                            <div id="kt_card_widget_17_chart" style="min-width: 70px; min-height: 70px" data-kt-size="70" data-kt-line="11"></div>
+                        </div>
+                        <!--end::Chart-->
+                        <!--begin::Labels-->
+                        <div class="d-flex flex-column content-justify-center flex-row-fluid">
+                            <!--begin::Label-->
+                            <div class="d-flex fw-semibold align-items-center">
+                                <!--begin::Bullet-->
+                                <div class="bullet w-8px h-3px rounded-2 bg-success me-3"></div>
+                                <!--end::Bullet-->
+                                <!--begin::Label-->
+                                <div class="text-gray-500 flex-grow-1 me-4">{{__('Deposit')}}</div>
+                                <!--end::Label-->
+                                <!--begin::Stats-->
+                                <div class="fw-bolder text-gray-700 text-xxl-end">{{session('currency').' '.$stats['deposit_transaction']}}</div>
+                                <!--end::Stats-->
+                            </div>
+                            <!--end::Label-->
+                            <!--begin::Label-->
+                            <div class="d-flex fw-semibold align-items-center my-3">
+                                <!--begin::Bullet-->
+                                <div class="bullet w-8px h-3px rounded-2 bg-primary me-3"></div>
+                                <!--end::Bullet-->
+                                <!--begin::Label-->
+                                <div class="text-gray-500 flex-grow-1 me-4">{{__('Withdraw')}}</div>
+                                <!--end::Label-->
+                                <!--begin::Stats-->
+                                <div class="fw-bolder text-gray-700 text-xxl-end">{{session('currency').' '.$stats['withdraw_transaction']}}</div>
+                                <!--end::Stats-->
+                            </div>
+                            <!--end::Label-->
+                        </div>
+                        <!--end::Labels-->
+                    </div>
+                    <!--end::Card body-->
+                </div>
+                <!--end::Card widget 3-->
                 <!--begin::Card widget 3-->
                 <div class="card card-flush h-md-50 mb-5 mb-xl-10">
                     <!--begin::Header-->
@@ -145,72 +280,6 @@
                     <!--end::Card body-->
                 </div>
                 <!--end::Card widget 3-->
-
-                <!--begin::List widget 4-->
-                <div class="card card-flush h-lg-50">
-                    <!--begin::Header-->
-                    <div class="card-header pt-5">
-                        <!--begin::Title-->
-                        <h3 class="card-title align-items-start flex-column">
-                            <span class="card-label fw-bold text-dark">{{__('Highlights')}}</span>
-                            <span class="text-gray-400 mt-1 fw-semibold fs-6">{{__('30 days financials')}}</span>
-                        </h3>
-                        <!--end::Title-->
-                    </div>
-                    <!--end::Header-->
-                    <!--begin::Body-->
-                    <div class="card-body pt-5">
-                        <!--begin::Item-->
-                        <div class="d-flex flex-stack">
-                            <!--begin::Section-->
-                            <div class="text-gray-700 fw-semibold fs-6 me-2">{{__('Income')}}</div>
-                            <!--end::Section-->
-                            <!--begin::Statistics-->
-                            <div class="d-flex align-items-senter">
-                                <!--begin::Number-->
-                                <span class="text-gray-900 fw-bolder fs-6">{{session('currency').' '.$stats['highlights']['income']}}</span>
-                            </div>
-                            <!--end::Statistics-->
-                        </div>
-                        <!--end::Item-->
-                        <!--begin::Separator-->
-                        <div class="separator separator-dashed my-3"></div>
-                        <!--end::Separator-->
-                        <!--begin::Item-->
-                        <div class="d-flex flex-stack">
-                            <!--begin::Section-->
-                            <div class="text-gray-700 fw-semibold fs-6 me-2">{{__('Expenses')}}</div>
-                            <!--end::Section-->
-                            <!--begin::Statistics-->
-                            <div class="d-flex align-items-senter">
-                                <!--begin::Number-->
-                                <span class="text-gray-900 fw-bolder fs-6">{{session('currency').' '.$stats['highlights']['expense']}}</span>
-                                <!--end::Number-->
-                            </div>
-                            <!--end::Statistics-->
-                        </div>
-                        <!--end::Item-->
-                        <!--begin::Separator-->
-                        <div class="separator separator-dashed my-3"></div>
-                        <!--end::Separator-->
-                        <!--begin::Item-->
-                        <div class="d-flex flex-stack">
-                            <!--begin::Section-->
-                            <div class="text-gray-700 fw-semibold fs-6 me-2">{{__('Referrals')}}</div>
-                            <!--end::Section-->
-                            <!--begin::Statistics-->
-                            <div class="d-flex align-items-senter">
-                                <!--begin::Number-->
-                                <span class="text-gray-900 fw-bolder fs-6">{{$stats['highlights']['referrals']}}</span>
-                                <!--end::Number-->
-                            </div>
-                            <!--end::Statistics-->
-                        </div>
-                        <!--end::Item-->
-                    </div>
-                    <!--end::Body-->
-                </div>
-                <!--end::LIst widget 4-->
             </div>
             <!--end::Col-->
 
